a11635b7ebcd4746d2f26ca036bacabee1d821c6
crystian97 <icrystianveloso@gmail.com>
2023-03-13 10:57:08 -0300

    feat:initial project structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>weather app</p>
+    </div>
+  );
+}
+
+export default App;
