dc042b11ec20ec29429b473e95b17062f9973fb6
crystian97 <icrystianveloso@gmail.com>
2023-03-14 15:39:36 -0300

    feat:add initial structure app without routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <p>weather app</p>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
